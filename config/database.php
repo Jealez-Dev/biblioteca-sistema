e0d0b213110cf14537cbc08754cec1a3d5eb6188
Jeanniel <jeanniel.zelaznog@gmail.com>
2026-02-14 08:20:19 -0400

    primero inicios

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "driver"    =>"mysql",
+    "host"      =>"localhost",
+    "user"      =>"root",
+    "pass"      =>"223366",
+    "database"  =>"noticias",
+    "charset"   =>"utf8"
+);
+?>
---
+++ views/Layouts/banner.php
@@ -0,0 +1,9 @@
+<div class="container">
+	<div class= "row" align="center">
+		        <div class="alert col-12" align="center">
+	   			     <img width="100%" src="../ejercicio/imagenes/BannerTeleMedicina.png" alt="Logo de TELEMEDICINA">
+				</div>
+		
+	
+	</div>
+</div>
