87ced8356d21678c80b6a4839ba4ae012f249236
Jeanniel <jeanniel.zelaznog@gmail.com>
2026-02-15 18:13:43 -0400

    solucione error de integridad: profe no combines 3 tecnologias rancias

--- views/Layouts/layout.php
+++ views/Layouts/layout.php
@@ -47,8 +47,8 @@
 
 		    <!-- Optional JavaScript -->
 		    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		    <script src="../biblioteca-sistema/js/jquery-3.3.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		    <script src="../biblioteca-sistema/js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		    <script src="../biblioteca-sistema/js/jquery-3.3.1.min.js"></script>
+		    <script src="../biblioteca-sistema/js/popper.min.js"></script>
 		    <script src="../biblioteca-sistema/js/bootstrap.min.js"></script>
 
 <footer>
